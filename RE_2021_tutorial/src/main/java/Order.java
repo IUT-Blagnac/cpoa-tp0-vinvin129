57f1d51e6a3f38644f5ac9c0070bf523027f8a89
vinvin129 <59414444+vinvin129@users.noreply.github.com>
2021-11-08 12:30:23 +0100

    TP0

---
+++ RE_2021_tutorial/src/main/java/Order.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Order {
+    private List<Drink> contents = new LinkedList<>();
+
+
+    private String owner;
+    public void setOwner(String who) { this.owner = who; }
+
+    private String recipient;
+    public void setRecipient(String who) { this.recipient = who; }
+
+    @Override
+    public String toString() {
+        return "Order: " + owner + " / " + recipient + " / { " + contents + "}";
+    }
+    public List<Drink> getDrinks() { return contents; }
+
+
+
+    static class Drink {
+        public Drink(String name){ this.name = name; }
+        private String name;
+        public String getName() { return name; }
+
+        @Override
+        public String toString() { return name; }
+    }
+
+}
